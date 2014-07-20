6cdee1ecb5aedee8140b670ed8b55c6e89e365bc
7u83 <7u83@mail.ru>
2014-07-20 08:00:14 +0200

    A little script to setup environment to compile for OpenWrt

---
+++ tools/openvrtenv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Setup evironement to compile for OpenWrt
+# You have probably to change the settings for STAGING_DIR and INCLUDE_DIR
+#
+
+export STAGING_DIR=`readlink -m ../openwrt/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2`
+export INCLUDE_DIR=`readlink -m ../openwrt/staging_dir/target-mips_r2_uClibc-0.9.33.2/usr/include`
+
+export PATH=$PATH:$STAGING_DIR/bin
+export CC=mips-openwrt-linux-uclibc-gcc
+export LD=mips-openwrt-linux-uclibc-ld
+export AR=mips-openwrt-linux-uclibc-ar
+
+/bin/bash
+
