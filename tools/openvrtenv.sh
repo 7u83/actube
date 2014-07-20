7649e6cfb82694b3d2ac2eb4feb67b12139890fe
7u83 <7u83@mail.ru>
2014-07-20 16:02:44 +0200

    Some improvemments

--- tools/openvrtenv.sh
+++ tools/openvrtenv.sh
@@ -7,6 +7,7 @@
 
 export STAGING_DIR=`readlink -m ../openwrt/staging_dir/toolchain-mips_r2_gcc-4.6-linaro_uClibc-0.9.33.2`
 export INCLUDE_DIR=`readlink -m ../openwrt/staging_dir/target-mips_r2_uClibc-0.9.33.2/usr/include`
+export LIB_DIR=`readlink -m ../openwrt/staging_dir/target-mips_r2_uClibc-0.9.33.2/usr/lib`
 
 export PATH=$PATH:$STAGING_DIR/bin
 export CC=mips-openwrt-linux-uclibc-gcc
