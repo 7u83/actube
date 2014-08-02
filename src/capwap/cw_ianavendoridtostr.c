8d34533f09f872a8398ae8d849c8014b74a991f7
7u83 <7u83@mail.ru>
2014-08-02 09:44:26 +0200

    Vendor string for FSF added.

--- src/capwap/cw_ianavendoridtostr.c
+++ src/capwap/cw_ianavendoridtostr.c
@@ -6,6 +6,8 @@ const char * cw_ianavendoridtostr(int id){
 	switch(id){
 		case 890:
 			return "ZyXEL Communications Corp.";
+		case 11591:
+			return "Free Software Foundation";
 		default:
 			return "Unknown";
 	}
