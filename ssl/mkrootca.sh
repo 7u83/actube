e5b45a269417516392c3f9991f73a10b4bdc700c
7u83 <7u83@mail.ru>
2014-08-10 21:32:50 +0200

    Rott ca creation in a separate file.

---
+++ ssl/mkrootca.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo Creating Root CA 
+openssl genrsa -des3 -out root-ca.key 1536 -config openssl.cnf
+openssl req -new -x509 -days 3650 -key root-ca.key -out root-ca.crt -config openssl.cnf
+
