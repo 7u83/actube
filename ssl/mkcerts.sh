19b49aa95fe3fc9fe21a42406d05da26be1c603e
7u83@mail.ru <7u83@mail.ru@noemail.net>
2014-08-11 15:31:04 +0000

    Played around with different startdate and enddate.
    FossilOrigin-Name: 7ccc7cfd165a1324a8b41bab7593c4641f7ee7da460eb1efe9b93ac878a43c75

--- ssl/mkcerts.sh
+++ ssl/mkcerts.sh
@@ -6,7 +6,8 @@ touch index.txt
 
 echo Creating AC Req
 openssl req -newkey rsa:1024 -keyout ac.key -config openssl.cnf -out ac.req
-openssl ca -config openssl.cnf  -out ac.crt -infiles ac.req
+#openssl ca -startdate 700101000000 -enddate 300101000000 -config openssl.cnf  -out ac.crt -infiles ac.req 
+openssl ca -config openssl.cnf  -out ac.crt -infiles ac.req 
 
 echo Creating Wtp Cert
 openssl req -newkey rsa:1024 -keyout wtp.key -config openssl.cnf -out wtp.req
