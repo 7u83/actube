acfb9f4665084298f8027535abfe0364d0beed63
7u83@mail.ru <7u83@mail.ru@noemail.net>
2014-08-14 15:18:19 +0000

    Cisco's vendor specific capwap payloads.
    FossilOrigin-Name: abe05f3eb01fe5cd8150c55338b491ab7aa5c152e1ec62136e7c4e3baae6b253

---
+++ src/capwap/capwap_cisco.h
@@ -0,0 +1,11 @@
+#ifndef __CAPWAP_CISCO_H
+#define __CAPWAP_CISCO_H
+
+#define CWVENDOR_CISCO_AP_TIMESYNC		151
+
+
+
+
+extern void cwmsg_addelem_vendor_cisco_ap_timesync(struct cwmsg * cwmsg);
+
+#endif
