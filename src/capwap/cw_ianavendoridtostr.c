d936a21f0cdee4df60a6dfd1f78a609338ea217e
7u83 <7u83@mail.ru>
2014-08-14 11:43:21 +0200

    Changed vendor id numbers to vender id defninitions, defined in capwap.h.

--- src/capwap/cw_ianavendoridtostr.c
+++ src/capwap/cw_ianavendoridtostr.c
@@ -1,14 +1,32 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 
 #include "capwap.h"
 
 /* definition of some iana assigned vendor id's */
 const char * cw_ianavendoridtostr(int id){
 	switch(id){
-		case 890:
+		case CW_VENDOR_ID_ZYXEL:
 			return "ZyXEL Communications Corp.";
-		case 11591:
+		case CW_VENDOR_ID_FSF:
 			return "Free Software Foundation";
-		case 4232704:
+		case CW_VENDOR_ID_CISCO:
 			return "Cisco";
 		default:
 			return "Unknown";
