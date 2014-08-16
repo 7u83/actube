773af35aa0010141fc44a936200bf4a977dd6418
7u83 <7u83@mail.ru>
2014-08-16 19:29:08 +0200

    Initial commmit.

---
+++ src/capwap/cwsend_image_data_response.c
@@ -0,0 +1,34 @@
+
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "sock.h"
+#include "capwap.h"
+#include "cw_log.h"
+
+
+void cwsend_image_data_response(struct conn * conn,int seqnum, int rc)
+{
+	cw_dbg(DBG_CW_MSG,"Sending imag data response to %s, seq = %d",sock_addr2str(&conn->addr),seqnum);
+
+	struct cwmsg * cwmsg = &conn->swm;	
+	cwmsg_init(cwmsg,conn->buffer,CWMSG_IMAGE_DATA_RESPONSE,seqnum,NULL);
+
+	cwmsg_addelem_result_code(cwmsg,rc);
+
+}
