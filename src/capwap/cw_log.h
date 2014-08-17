5ab147e01e98cbd876fca83db3bf6ffbcdc6e287
7u83@mail.ru <7u83@mail.ru@noemail.net>
2014-08-17 09:51:33 +0000

    Fixed ...
    FossilOrigin-Name: 4707126029889b0a8361da17088977020f6a2ac7da5c5c6748076d77d9e2f64f

--- src/capwap/cw_log.h
+++ src/capwap/cw_log.h
@@ -67,7 +67,7 @@ extern int cw_dbg_opt_detail;
 extern int cw_dbg_opt_level;
 
 
-#define cw_dbg_is_level(type) (cw_dbg_opt_type & level)
+#define cw_dbg_is_level(level) (cw_dbg_opt_level & level)
 
 
 
