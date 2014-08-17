a2b59eccd72bfca842981488ecc6f32fd7e6feb0
7u83@mail.ru <7u83@mail.ru@noemail.net>
2014-08-17 22:03:07 +0000

    Initial commit
    FossilOrigin-Name: 703aa1ad65247f68e1fee3a8aec01c44e6dae819b8eaa74cb777e220d1ed1fc9

---
+++ src/capwap/cw_readelem_statistics_timer.c
@@ -0,0 +1,18 @@
+#include "capwap.h"
+
+#include "cw_log.h"
+
+int cw_readelem_statistics_timer(uint16_t *timer, int type, uint8_t * msgelem, int len)
+{
+	if (type != CWMSGELEM_STATISTICS_TIMER)
+		return 0;
+
+	if (len!=2){
+		cw_dbg(DBG_CW_RFC,"Statistics timer msgelem has wrong size, type=%d,len=%d",type,len);
+		return 0;
+	}
+	*timer=*((uint16_t*)msgelem);
+	return 1;
+}
+
+
