6a07f94f16c345d873e3e0b7a948f22226a46d7e
tube@tube-ThinkPad-X100e.(none) <tube@tube-ThinkPad-X100e.(none)@noemail.net>
2014-08-19 17:12:31 +0000

    Download and compile libuci.
    FossilOrigin-Name: ed56b418221d09ee07c480fcc16aa5661284ddcf6e598bdc85301960eabde17b

---
+++ src/contrib/install_libuci.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+git clone git://nbd.name/luci2/libubox.git libubox
+cd libubox
+mkdir build
+cd build
+cmake ..
+make ubox
+cd ../..
+
+git clone git://nbd.name/uci.git uci
+cd uci
+mkdir build
+cd build
+
+CFLAGS=-I../../
+LDFLAGS=-L../../libubox/build/
+export CFLAGS
+export LDFLAGS
+
+cmake ..
+make uci cli
+
