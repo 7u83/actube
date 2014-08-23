cd2b70d89404e8e6c975756134f9319b8dd6e290
7u83 <7u83@mail.ru>
2014-08-23 12:00:05 +0200

    Initial commit.

---
+++ src/capwap/conn_prepare_configuration_update_request.c
@@ -0,0 +1,13 @@
+
+
+#include "conn.h"
+#include "capwap.h"
+
+
+void conn_prepare_configuration_update_request(struct conn * conn)
+{
+	struct cwmsg * cwmsg = &conn->req_msg;
+	uint8_t * buffer = conn->req_buffer;
+	cwmsg_init(cwmsg,buffer,CWMSG_CONFIGURATION_UPDATE_REQUEST,conn_get_next_seqnum(conn),0);
+
+}
