a0fb722d1207fd869181aa89b0319dd8a0c8da0b
7u83@mail.ru <7u83@mail.ru@noemail.net>
2014-08-23 14:05:51 +0000

    Sets vendor id correctly.
    FossilOrigin-Name: a019b5230da5cc38db55d74e52e6d1aef5e83fb3c568f8821dc58cba088745f7

--- src/capwap/cwmsg_addelem_image_identifier.c
+++ src/capwap/cwmsg_addelem_image_identifier.c
@@ -2,14 +2,9 @@
 #include "capwap.h"
 #include "cwmsg.h"
 
-#include <string.h> //tube
-
 void cwmsg_addelem_image_identifier(struct cwmsg *cwmsg,uint32_t vendor_id,uint8_t *img, int len)
 {
-//	uint32_t net_vendor_id = htonl(vendor_id);
-//	cwmsg_vaddelem(cwmsg,CWMSGELEM_IMAGE_IDENTIFIER,2,&net_vendor_id,sizeof(net_vendor_id),img,len);
-
-	uint8_t name[]="/tobias_heere\0";
-	cwmsg_addelem(cwmsg,CWMSGELEM_IMAGE_IDENTIFIER,name,strlen((char*)name)+1);
+	uint32_t net_vendor_id = htonl(vendor_id);
+	cwmsg_vaddelem(cwmsg,CWMSGELEM_IMAGE_IDENTIFIER,2,&net_vendor_id,sizeof(net_vendor_id),img,len);
 }
 
