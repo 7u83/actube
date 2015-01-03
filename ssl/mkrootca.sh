ad34954787aae4a5fdbed45a60ce9654cfbefe1c
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-01-03 21:18:01 +0000

    Changed days to days/10
    FossilOrigin-Name: f3e5531615f44c27f85ae218462f9b11c387bef255abedf38f9c52fd9e80a400

--- ssl/mkrootca.sh
+++ ssl/mkrootca.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo Creating Root CA 
 openssl genrsa -des3 -out root-ca.key 1536 -config openssl.cnf
-openssl req -new -x509 -days 33650 -key root-ca.key -out root-ca.crt -config openssl.cnf 
+openssl req -new -x509 -days 3365 -key root-ca.key -out root-ca.crt -config openssl.cnf 
 
 
