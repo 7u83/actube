48a4ab6b8c5eb2ee61c0b580163f65e81d8a34e8
7u83 <7u83@mail.ru>
2015-01-22 23:32:18 +0100

    Creates on certificate

--- ssl/mkcerts.sh
+++ ssl/mkcerts.sh
@@ -1,15 +1,3 @@
 #!/bin/sh
-rm index.*
-rm serial.*
-echo '1000' > serial
-touch index.txt
 
-echo Creating AC Req
-openssl req -newkey rsa:1024 -keyout ac.key -config openssl.cnf -out ac.req
-#openssl ca -startdate 700101000000 -enddate 300101000000 -config openssl.cnf  -out ac.crt -infiles ac.req 
-openssl ca -config openssl.cnf  -out ac.crt -infiles ac.req 
-
-echo Creating Wtp Cert
-openssl req -newkey rsa:1024 -keyout wtp.key -config openssl.cnf -out wtp.req
-openssl ca -config openssl.cnf -out wtp.crt -infiles wtp.req
-
+./mkcert.sh ac
