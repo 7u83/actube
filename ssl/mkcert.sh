1e884be4f1f7f99435b9f417e0b747aa4ceb1ce6
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-01-24 10:05:54 +0000

    Added option for "simple" certificate
    "simple" means, issued certificates are directly signed
    by root CA, no chaining.
    
    FossilOrigin-Name: 79ee157b5b2e7bb9529049f646d882a0d1aac58c01b4f7d0438d4948faa4f0f6

--- ssl/mkcert.sh
+++ ssl/mkcert.sh
@@ -2,16 +2,24 @@
 
 KEYSIZE=2048
 
+#set -x
 
 NAME=$1
-
 PREF=$2
+
+#if [ ! -z $2 ]
+#then
+#	PREF=""
+#fi
+
+
 SUBJ="/C=DE/ST=Berlin/L=Berlin/O=Cauwersin/CN=7u83.cauwersin.com/emailAddress=7u83@mail.ru"
-if [ $2 = "cisco" ] 
+if [ "$PREF" = "cisco" ] 
 then
 	PREF="$2-"
 	SUBJ="/C=US/ST=California/L=San Jose/O=Cisco Virtual Wireless LAN Controller/CN=DEVICE-AC-TUBE/emailAddress=7u83@mail.ru"
 fi
+
 
 
 
@@ -30,17 +38,26 @@ fi
 openssl req -nodes -newkey rsa:$KEYSIZE -keyout $DIR/$NAME.key -out $DIR/$NAME.req   \
         -subj "$SUBJ"
 
+if [ "$PREF" = "simple" ]
+then
+openssl ca -config openssl-simple.cnf  \
+	   -keyfile $ROOT_CA_DIR/${PREF}-root-ca.key \
+	   -cert $ROOT_CA_DIR/${PREF}-root-ca.crt \
+	   -batch \
+	   -out $DIR/$NAME.crt -infiles $DIR/$NAME.req 
+
+else
 openssl ca -config openssl-int.cnf  \
 	   -keyfile $INT_CA_DIR/${PREF}int-ca.key \
 	   -cert $INT_CA_DIR/${PREF}int-ca.crt \
 	   -batch \
 	   -out $DIR/$NAME.crt -infiles $DIR/$NAME.req 
-	
+cat $INT_CA_DIR/${PREF}int-ca.crt >> $DIR/$NAME.pem
+cat $ROOT_CA_DIR/${PREF}root-ca.crt >> $DIR/$NAME.pem
+fi	
 
 openssl x509 -in $DIR/$NAME.crt -out $DIR/$NAME.pem
 
-cat $INT_CA_DIR/${PREF}int-ca.crt >> $DIR/$NAME.pem
-cat $ROOT_CA_DIR/${PREF}root-ca.crt >> $DIR/$NAME.pem
 
 
 
