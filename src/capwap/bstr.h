446d6308d0f3fddeedfc8e4881108caa6f1facc7
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-01-31 11:29:27 +0000

    Function behaviour changed.
    FossilOrigin-Name: 09a2a2413e6ddbf1b4b5350fe443c7036ad17e6e118cb9fc44da4555393a5d32

--- src/capwap/bstr.h
+++ src/capwap/bstr.h
@@ -3,5 +3,8 @@
 
 
 extern uint8_t * bstr_create(uint8_t *data, uint8_t len);
+extern uint8_t * bstr_create_from_cfgstr(const char * s);
+extern uint8_t * bstr_replace( uint8_t ** dst, uint8_t * bstr);
+
 
 #define bstr_len(s) (*s)
