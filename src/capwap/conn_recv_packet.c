417dfb70437bc77c0b3784f1b6c6af543e9e7ed2
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-02-08 13:01:35 +0000

    Removed some printfs.
    FossilOrigin-Name: 01f2e28c77bc000c68c39df1b3221d76eef1b2da8d57a569d35c40a5679c46d0

--- src/capwap/conn_recv_packet.c
+++ src/capwap/conn_recv_packet.c
@@ -11,25 +11,14 @@
 
 int conn_recv_packet(struct conn* conn,uint8_t *buf,int len)
 {
-	printf("THHHHHHHHHHHHHHHHHHHHHHHHHe  Conn recv packet called\n");
 	int n;
 	int flags=0;
 	while( (n = recv(conn->sock,(char*)buf,len,flags)) < 0 ){
 		if (errno!=EINTR)
 		{
-			if (errno == EWOULDBLOCK)
-			{
-				printf("would block\n");
-				
-//				continue;
-			}
 			if ( errno==EAGAIN )
-			{
-				printf("again\n");
-			}
-			perror("recv");
-		printf("Return n %d\n",n);	
-		return n;
+				return n;
+
 		}
 	}
 	return n;
