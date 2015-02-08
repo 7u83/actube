7264630ca83c5ad92e2c473c0747bffaddad7699
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-02-08 15:58:08 +0000

    Copyright notice added.
    FossilOrigin-Name: b62ea216fa0e03ba7730a10b5a7b3fbc7aa77276d7e0d1b772452b3369fad35f

--- src/capwap/conn_recv_packet.c
+++ src/capwap/conn_recv_packet.c
@@ -1,3 +1,21 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -5,9 +23,6 @@
 #include <string.h>
 
 #include "conn.h"
-
-
-#include <stdio.h>
 
 int conn_recv_packet(struct conn* conn,uint8_t *buf,int len)
 {
