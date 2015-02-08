b7409350fa6521c5621c11a486586b2a058b88b8
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-02-08 15:28:04 +0000

    dtls_gnutls functions sorted into their appropriate files.
    FossilOrigin-Name: a30eb4c7530df491e7ce4debaffe57aac01af394c2d82481fe38f1661c5d6e23

--- src/capwap/dtls_gnutls.c
+++ src/capwap/dtls_gnutls.c
@@ -16,15 +16,128 @@
 
 */
 
+#include <stdlib.h>
 
 #include <gnutls/gnutls.h>
 
+#include "dtls_gnutls.h"
+
+#include "conn.h"
 #include "cw_log.h"
+#include "cw_util.h"
 
 int dtls_gnutls_init()
 {
 	cw_dbg(DBG_CW_INFO,"Init SSL library - using GnuTLS %s",gnutls_check_version(NULL));
 	gnutls_global_init();
-
 	return 1;
 }
+
+
+void dtls_gnutls_data_destroy(struct dtls_gnutls_data *d)
+{
+	free(d);
+}
+
+int dtls_gnutls_write(struct conn * conn, const uint8_t *buffer, int len)
+{
+	struct dtls_gnutls_data * d = conn->dtls_data;
+
+	int rc = gnutls_record_send(d->session,buffer,len);
+
+	if ( rc < 0 ){
+		cw_log(LOG_ERR, "DTLS - write error: %s", gnutls_strerror(rc));
+		conn->dtls_error=1;
+		return -1;
+	}
+
+	return rc;
+}
+
+
+
+int dtls_gnutls_read(struct conn * conn, uint8_t *buffer, int len)
+{
+	struct dtls_gnutls_data * d = conn->dtls_data;
+	int rc = gnutls_record_recv(d->session,buffer,len);
+
+	if ( rc == GNUTLS_E_AGAIN )
+		return 0;
+
+	if ( rc < 0 ){
+		cw_log(LOG_ERR, "DTLS - read error: %s", gnutls_strerror(rc));
+		conn->dtls_error=1;
+		return -1;
+	}
+
+	return rc;
+}
+
+
+struct dtls_gnutls_data *dtls_gnutls_data_create(struct conn *conn)
+{
+	struct dtls_gnutls_data *d = malloc(sizeof(struct dtls_gnutls_data));
+	if (!d)
+		return 0;
+
+	gnutls_certificate_allocate_credentials(&d->x509_cred);
+
+
+	int rc;
+
+	/* Set credentials */
+	rc = gnutls_certificate_set_x509_key_file(d->x509_cred, conn->dtls_cert_file,
+						  conn->dtls_key_file, GNUTLS_X509_FMT_PEM);
+
+	if (rc < 0) {
+		cw_log(LOG_ERR, "DTLS - Can't set cert/key: %s", gnutls_strerror(rc));
+		dtls_gnutls_data_destroy(d);
+		return 0;
+	}
+
+	/* Set ciphers */
+	const char *errpos;
+	rc = gnutls_priority_init(&d->priority_cache, conn->dtls_cipher, &errpos);
+	if (rc < 0) {
+		cw_log(LOG_ERR, "DTLS - Can't init ciphers '%s' at '%s' : %s", conn->dtls_cipher,
+		       errpos, gnutls_strerror(rc));
+		dtls_gnutls_data_destroy(d);
+		return 0;
+	}
+
+
+	rc = gnutls_init(&d->session, GNUTLS_SERVER | GNUTLS_DATAGRAM);
+	if (rc < 0) {
+		cw_log(LOG_ERR, "DTLS - Can't init session: %s", gnutls_strerror(rc));
+		dtls_gnutls_data_destroy(d);
+		return 0;
+	}
+
+	gnutls_transport_set_ptr(d->session, conn);
+
+
+	rc = gnutls_priority_set(d->session, d->priority_cache);
+	if (rc < 0) {
+		cw_log(LOG_ERR, "DTLS - Can't set priority: %s", gnutls_strerror(rc));
+		dtls_gnutls_data_destroy(d);
+		return 0;
+	}
+
+
+	rc = gnutls_credentials_set(d->session, GNUTLS_CRD_CERTIFICATE, d->x509_cred);
+
+	if (rc < 0) {
+		cw_log(LOG_ERR, "DTLS - Can't set credentials: %s", gnutls_strerror(rc));
+		dtls_gnutls_data_destroy(d);
+		return 0;
+	}
+
+	gnutls_certificate_server_set_request(d->session,GNUTLS_CERT_REQUEST);
+
+	gnutls_transport_set_pull_function(d->session, dtls_gnutls_bio_read);
+	gnutls_transport_set_push_function(d->session, dtls_gnutls_bio_write);
+
+	return d;
+}
+
+
--- src/capwap/dtls_gnutls.h
+++ src/capwap/dtls_gnutls.h
@@ -16,6 +16,10 @@
 
 */
 
+/*
+ * GnuTLS specific definitions for DTLS
+ */ 
+
 #ifndef __DTLS_GNUTLS_H
 #define __DTLS_GNUTLS_H
 
@@ -23,13 +27,28 @@
 
 #include "conn.h"
 
-
+/* "public" functions */
 extern int dtls_gnutls_init();
 extern int dtls_gnutls_accept(struct conn * conn);
+extern int dtls_gnutls_connect(struct conn * conn);
 extern const char * dtls_gnutls_get_cipher(struct conn * conn);
+
+
+/* functions used only by capwap libray */
 
 extern ssize_t dtls_gnutls_bio_read(gnutls_transport_ptr_t b, void *out, size_t maxlen);
 extern ssize_t dtls_gnutls_bio_write(gnutls_transport_ptr_t b, const void *data, size_t len);
+extern int dtls_gnutls_bio_wait(gnutls_transport_ptr_t ptr, unsigned int ms);
+extern int dtls_gnutls_read(struct conn * conn, uint8_t *buffer, int len);
+extern int dtls_gnutls_write(struct conn * conn, const uint8_t *buffer, int len);
+
+struct dtls_gnutls_data {
+	gnutls_session_t session;
+	gnutls_certificate_credentials_t x509_cred;
+	gnutls_priority_t priority_cache;
+};
+
+struct dtls_gnutls_data *dtls_gnutls_data_create(struct conn *conn);
 
 #endif
 
