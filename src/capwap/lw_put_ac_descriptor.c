cc9eb97cfdaa3f5a86a87469bc1b800989de188e
7u83 <7u83@mail.ru>
2015-03-23 21:20:09 +0100

    LWAPP stuff.

---
+++ src/capwap/lw_put_ac_descriptor.c
@@ -0,0 +1,19 @@
+#include <string.h>
+
+#include "acinfo.h"
+#include "lwapp.h"
+
+int lw_put_ac_descriptor(uint8_t * dst, struct ac_info * acinfo)
+{
+	uint8_t *d=dst;
+	d+=lw_put_byte(d,0); /* Cisco MWAR type */
+
+	d+=lw_put_bstr(d,acinfo->hardware_version); /* length must be 4 */
+	d+=lw_put_bstr(d,acinfo->software_version); /* length must be 4 */
+	d+=lw_put_word(d,200);
+	d+=lw_put_word(d,1000);
+	d+=lw_put_word(d,2);
+	d+=lw_put_word(d,2);
+	d+=lw_put_byte(d,acinfo->security);
+	return d-dst;
+}
