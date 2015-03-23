c1c4d74e652a040c3d99bda7bf046553b2e49cd4
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-03-23 06:49:26 +0000

    Update req added.
    FossilOrigin-Name: 00c7bf2866b0adedbbabfdd26daf829cc0f627e462a8e8750dade3b906967e8d

---
+++ src/capwap/cw_prepare_configuration_status_request.c
@@ -0,0 +1,28 @@
+
+#include "capwap.h"
+#include "lwapp_cisco.h"
+
+
+#include "conn.h"
+#include "wtpinfo.h"
+
+void cw_prepare_configuration_status_request(struct conn * conn, struct wtpinfo *wtpinfo)
+{
+	struct cwmsg * cwmsg = &conn->req_msg;
+	uint8_t * buffer = conn->req_buffer;
+	cwmsg_init(cwmsg,buffer,CW_MSG_CONFIGURATION_STATUS_REQUEST,conn_get_next_seqnum(conn),0);
+	cwmsg->capwap_mode=conn->capwap_mode;
+	
+	cwmsg_addelem_ac_name(cwmsg,(uint8_t *)"AC-iMaxi");	
+
+/*
+	uint8_t mtu[2048];
+	int l = lw_put_cisco_path_mtu(mtu,1485,1701);
+
+	printf("Len = %d\n",l);
+
+	cwmsg_addelem_vendor_specific_payload(cwmsg,LW_VENDOR_CISCO,
+		LW_ELEM_VENDOR_SPECIFIC,mtu,l);
+
+*/
+}
