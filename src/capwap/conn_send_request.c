f70288b8828850038b0a942dd022b71e7f684c9c
7u83 <7u83@mail.ru>
2015-03-28 08:39:48 +0100

    Comments improved.

--- src/capwap/conn_send_request.c
+++ src/capwap/conn_send_request.c
@@ -1,3 +1,21 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 /**
  *@file
  *@brief conn_send_request 
@@ -8,8 +26,10 @@
 #include "cw_log.h"
 
 /**
- * Send a request message and wait for response
+ * Send a request message and wait for its response
  * @param conn the connection
+ *
+ * The message has to be prepared and put to conn->req_msg
  * 
  */ 
 struct cwrmsg * conn_send_request(struct conn * conn)
