8bfc4fb2a1fa43595b56075216231de08f4ad933
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-03-28 01:12:27 +0000

    Inital commit
    FossilOrigin-Name: 386b4475560119c9158d672a977fb99b1671d0853ee5bd439e18c64d5fd3ce97

---
+++ src/capwap/lw_put_80211_wtp_wlan_radio_configuration.c
@@ -0,0 +1,23 @@
+
+#include "lwapp.h"
+
+int lw_put_80211_wtp_wlan_radio_configuration(uint8_t*dst,struct radioinfo *ri)
+{
+	lw_put_byte(dst,ri->rid);
+	lw_put_byte(dst+1,0);
+	lw_put_word(dst+2,ri->occupancy_limit);
+	lw_put_byte(dst+4,ri->cfp_period);
+	lw_put_word(dst+5,ri->cfp_max_duration);
+
+	/* XXX catch rmac shorter or longer than 6*/
+
+	lw_put_bstr(dst+7,ri->rmac);	/* length MUST be 6 */
+	
+	lw_put_word(dst+13,ri->beacon_period);
+	lw_put_byte(dst+15,ri->dtim_period);
+	lw_put_data(dst+16,ri->country_str,4);
+	lw_put_byte(dst+20,ri->max_bssid);
+
+	return 21;	
+}
+
