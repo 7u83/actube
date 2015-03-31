6bd120ba38362a7e90c9d2c1ad4cd9269bc174e9
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-03-31 12:19:31 +0000

    Namiing
    FossilOrigin-Name: ad113ff389cc4007f0f94cb1df2ce40513cfb30a6db5652a4f3862ebd91ce2ce

---
+++ src/capwap/capwap_strings_msg.c
@@ -0,0 +1,37 @@
+#include "capwap.h"
+
+struct cw_strlist capwap_strings_msg[] = {
+{ CW_MSG_DISCOVERY_REQUEST, "Discovery Request" },
+{ CW_MSG_DISCOVERY_RESPONSE,"Discovery Response" },
+{ CW_MSG_JOIN_REQUEST, "Join Request"},
+{ CW_MSG_JOIN_RESPONSE,"Join Response"},
+{ CW_MSG_CONFIGURATION_STATUS_REQUEST,"Configuration Request"},
+{ CW_MSG_CONFIGURATION_STATUS_RESPONSE,"Configuration Response"},
+{ CW_MSG_CONFIGURATION_UPDATE_REQUEST,"Update Request"},
+{ CW_MSG_CONFIGURATION_UPDATE_RESPONSE,"Update Response"},
+{ CW_MSG_WTP_EVENT_REQUEST,"WTP Event Request"},
+{ CW_MSG_WTP_EVENT_RESPONSE,"WTP Event Response"},
+{ CW_MSG_CHANGE_STATE_EVENT_REQUEST,"Change State Event Request"},
+{ CW_MSG_CHANGE_STATE_EVENT_RESPONSE,"Change State Event Response"},
+{ CW_MSG_ECHO_REQUEST,"Echo Request"},
+{ CW_MSG_ECHO_RESPONSE,"Echo Response"},
+{ CW_MSG_IMAGE_DATA_REQUEST,"Image Data Request"},
+{ CW_MSG_IMAGE_DATA_RESPONSE,"Image Data Response"},
+{ CW_MSG_RESET_REQUEST,"Reset Request"},
+{ CW_MSG_RESET_RESPONSE,"Reset Response"},
+{ CW_MSG_PRIMARY_DISCOVERY_REQUEST,"Primary Discovery Request"},
+{ CW_MSG_PRIMARY_DISCOVERY_RESPONSE,"Primary Discovery Response"},
+
+/*           Data Transfer Request               21
+           Data Transfer Response              22
+           Clear Configuration Request         23
+           Clear Configuration Response        24
+           Station Configuration Request       25
+           Station Configuration Response      26
+*/
+
+{0,"Unknown Message Type"}
+
+};
+
+
