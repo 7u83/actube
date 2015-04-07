3915bf5787ced7f7681a8ea6c300a8153c2c701c
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-04-07 12:54:26 +0000

    Added comments.
    FossilOrigin-Name: 63db9e0973255068703930ea26785ae0bcd6f4e103a66d5b0233e11ae117b008

--- src/capwap/md5sum.c
+++ src/capwap/md5sum.c
@@ -6,10 +6,17 @@
 
 #include "capwap_crypto.h"
 
-
 #define BLOCK_SIZE 4096
 
-
+/**
+ * Calculate MD5 checksum for an opened file. 
+ * @param digest destination buffer for calculated 
+ *   checksum (16 bytes, for a predifned constant use #CW_MD5_DIGEST_SIZE)
+ * @param infile filehanle 
+ * 
+ * Remember to set the file pointer to the beginning of the file, before
+ * calling this function. Therefor use fseek(file,0,SEEK_SET).
+ */
 int cw_fgetmd5sum(uint8_t *digest, FILE *infile)
 {
 	struct md5_ctx ctx;
