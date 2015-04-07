20ec429dcc517209b4328feebc6a052017db5bdb
7u83 <7u83@mail.ru>
2015-04-07 14:54:45 +0200

    Added commemnts.

--- src/capwap/capwap_crypto.h
+++ src/capwap/capwap_crypto.h
@@ -2,8 +2,14 @@
 #define __CAPWAP_CRYPTO_H
 
 #include <nettle/md5.h>
+/**
+ *@file
+ *@brief
+ */
 
+/** MD5 digest size, 16 bytes */
 #define CW_MD5_DIGEST_SIZE MD5_DIGEST_SIZE
+
 
 int cw_fgetmd5sum(uint8_t *digest, FILE *infile);
 
