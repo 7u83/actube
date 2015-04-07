01d5b87d2b2db503ff5d18f2d1c5ad0d509c738f
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-04-07 05:44:55 +0000

    Inital commit
    FossilOrigin-Name: 2b08b7701d9cf1bb3b54a86ab6900d2258513d01582a0338b615badf6b688144

---
+++ src/capwap/cw_in_check_disc_req.c
@@ -0,0 +1,29 @@
+
+#include "capwap.h"
+#include "intavltree.h"
+#include "cw_log.h"
+#include "dbg.h"
+#include "sock.h"
+
+int cw_in_check_disc_req(struct conn *conn, struct cw_action_in *a, uint8_t * data,
+			 int len)
+{
+
+	cw_action_in_t *mlist[20];
+	int n = cw_check_missing_mand(mlist, conn, a);
+
+	cw_dbg_missing_mand(DBG_ELEM, conn, mlist, n, a);
+
+	conn->capwap_state = CW_STATE_NONE;
+
+	/* if mandatory elements are missing send no discovery response */
+	if (n) {
+		cw_dbg(DBG_CW_INFO,
+		       "Ignoring Discovery Request from %s - missing mandatory elements.",
+		       sock_addr2str(&conn->addr));
+		return -1;
+	}
+
+	/* ok, send response */
+	return 0;
+}
