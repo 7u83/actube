d9db94f69aa98df658cafcc995a4dbd413e47b00
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-04-07 05:57:34 +0000

    Inital.
    FossilOrigin-Name: e4d22e7a36ee2cd6085c05b0aa4c5a8be39661a5d8a5dfb26cc37f085f5be0ef

---
+++ src/ac/ac.h
@@ -0,0 +1,8 @@
+#include "capwap/capwap.h"
+#include "capwap/itemstore.h"
+
+extern struct ac_status ac_global_status;
+extern int ac_global_init();
+extern cw_itemstore_t ac_config;	
+extern struct cw_actiondef capwap_actions;
+
