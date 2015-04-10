e7ac78f3e0a6f948e9a651c822becc7a0d4ce724
7u83 <7u83@mail.ru>
2015-04-10 08:43:42 +0200

    Added comments.

--- src/capwap/avliter_next.c
+++ src/capwap/avliter_next.c
@@ -1,6 +1,33 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
+ *@file 
+ *@brief Implementation of avliter_next
+ */
+
 #include "avltree.h"
 
-
+/**
+ * Get the next element within an AVL Tree.
+ * @param i pointer to AVL Iterator
+ * @return the element or NULL if there is no next elemeent.
+ */ 
 void * avliter_next(avliter_t *i)
 {
 
@@ -29,7 +56,6 @@ void * avliter_next(avliter_t *i)
 		
 		/* push node */
 		i->stack[i->stack_ptr++]=i->cur;
-
 
 		i->cur=i->cur->left;
 
