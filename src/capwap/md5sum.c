0943c85d29a59497bb7f4e69991372b8110ad2bf
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-04-10 21:48:41 +0000

    Inital...
    FossilOrigin-Name: 55b4ca3550d8548e848fe274c09f29019be1a660265b9abf586b28b3e8c2a547

--- src/capwap/md5sum.c
+++ src/capwap/md5sum.c
@@ -1,3 +1,24 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+/**
+ * @file
+ * @brief
+ */ 
 
 #include <stdio.h>
 #include <errno.h>
@@ -12,10 +33,10 @@
  * Calculate MD5 checksum for an opened file. 
  * @param digest destination buffer for calculated 
  *   checksum (16 bytes, for a predifned constant use #CW_MD5_DIGEST_SIZE)
- * @param infile filehanle 
+ * @param infile file hanle 
  * 
  * Remember to set the file pointer to the beginning of the file, before
- * calling this function. Therefor use fseek(file,0,SEEK_SET).
+ * calling this function. Therefore use fseek(file,0,SEEK_SET).
  */
 int cw_fgetmd5sum(uint8_t *digest, FILE *infile)
 {
