86c06c99b0a24174c3b32b6ce6406992b1c52666
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-04-13 08:59:47 +0000

    Inital commit.
    FossilOrigin-Name: 7c7ad097c2960ff4fc53e7bd60815f046b3be71a30a6781ae171cba7c0dd7027

---
+++ src/capwap/lw_put_image_data.c
@@ -0,0 +1,25 @@
+#include "lwapp.h"
+
+int lw_put_image_data(uint8_t *dst,FILE *infile)
+{
+	if ( feof(infile)){
+		lw_put_byte(dst+0,1);
+		return 1;
+	}
+
+	int bytes = fread(dst+3,1,LW_BLOCKSIZE_IMAGE_DATA,infile);
+
+	if ( ferror(infile)) {
+		lw_put_byte(dst+0,5);
+		return 1;
+	}
+
+	lw_put_byte(dst,3); 
+
+	uint16_t checksum = lw_checksum(dst+3,bytes);
+	lw_put_word(dst+1,checksum);
+		
+	return bytes+3;
+}
+
+
