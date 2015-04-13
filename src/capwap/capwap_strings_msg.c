abc101c57d591d44e723db75afb5512301eb8b08
7u83 <7u83@mail.ru>
2015-04-13 11:46:57 +0200

    Changed text for Config strings.

--- src/capwap/capwap_strings_msg.c
+++ src/capwap/capwap_strings_msg.c
@@ -1,14 +1,39 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+/**
+ * @file
+ * @brief Definition of CAPWAP Message Strings
+ */ 
+
 #include "capwap.h"
 
+/**
+ * CAPWAP Message Strings
+ */ 
 struct cw_str capwap_strings_msg[] = {
 { CW_MSG_DISCOVERY_REQUEST, "Discovery Request" },
 { CW_MSG_DISCOVERY_RESPONSE,"Discovery Response" },
 { CW_MSG_JOIN_REQUEST, "Join Request"},
 { CW_MSG_JOIN_RESPONSE,"Join Response"},
-{ CW_MSG_CONFIGURATION_STATUS_REQUEST,"Configuration Request"},
-{ CW_MSG_CONFIGURATION_STATUS_RESPONSE,"Configuration Response"},
-{ CW_MSG_CONFIGURATION_UPDATE_REQUEST,"Update Request"},
-{ CW_MSG_CONFIGURATION_UPDATE_RESPONSE,"Update Response"},
+{ CW_MSG_CONFIGURATION_STATUS_REQUEST,"Configuration Status Request"},
+{ CW_MSG_CONFIGURATION_STATUS_RESPONSE,"Configuration Status Response"},
+{ CW_MSG_CONFIGURATION_UPDATE_REQUEST,"Configuration Update Request"},
+{ CW_MSG_CONFIGURATION_UPDATE_RESPONSE,"Configuration Update Response"},
 { CW_MSG_WTP_EVENT_REQUEST,"WTP Event Request"},
 { CW_MSG_WTP_EVENT_RESPONSE,"WTP Event Response"},
 { CW_MSG_CHANGE_STATE_EVENT_REQUEST,"Change State Event Request"},
