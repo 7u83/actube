48ebb3cda56c758e05cf87a9f2ea8f6c0c09d234
7u83 <7u83@mail.ru>
2015-04-13 12:47:47 +0200

    Puts no image update req out.

--- src/capwap/cw_out_cisco_ac_descriptor.c
+++ src/capwap/cw_out_cisco_ac_descriptor.c
@@ -24,7 +24,7 @@ int cw_out_cisco_ac_descriptor(struct conn *conn,struct cw_action_out * a,uint8_
 	i = cw_itemstore_get(conn->local,CW_ITEM_AC_SOFTWARE_VERSION);
 
 	if ( i ) {	
-	 	d += cw_put_version(d,1,i->data);
+	 	d += cw_put_version(d,5,i->data);
 	}
 	else {
 		cw_log(LOG_ERR, "Can't set Software Version in AC descriptor, No value defined.");
