2bdab4182fb3d2b8a51ded8016088929d2b4b741
7u83 <7u83@mail.ru>
2015-04-19 16:50:45 +0200

    Inital.

---
+++ src/wtp/changestate.c
@@ -0,0 +1,19 @@
+#include "capwap/capwap.h"
+#include "capwap/conn.h"
+#include "wtp_interface.h"
+
+
+int changestate()
+{
+
+
+	struct conn * conn = get_conn();
+	int rc = cw_send_request(conn,CW_MSG_CHANGE_STATE_EVENT_REQUEST);
+
+	if ( !cw_rcok(rc) ) {
+		cw_strresult(rc);
+	}
+
+
+}
+
