c41ef3708a489e2f5579f6225024879d233f603d
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-04-19 14:46:09 +0000

    Inital.
    FossilOrigin-Name: 521647429af7188385f765cb130018690e70a4928f3746fde7a56c46dc0b4fcc

---
+++ src/capwap/item_strings.c
@@ -0,0 +1,16 @@
+
+#include "capwap_items.h"
+#include "strheap.h"
+
+struct cw_str cw_item_strings[] = {
+	{ CW_ITEM_WTP_HARDWARE_VERSION,"WTP Hardware Version"},
+	{ CW_ITEM_WTP_NAME, "WTP Name "},
+	{ CW_ITEM_AC_NAME, "AC Name "},
+	{ CW_ITEM_WTP_BOARD_VENDOR, "WTP Board Vendor"},
+	{ CW_ITEM_WTP_BOARD_MODELNO,"WTP Board Model No."},
+	{ CW_ITEM_WTP_BOARD_MACADDRESS, "WTP Board MAC Address"},
+	{ CW_ITEM_WTP_BOARD_ID,"WTP Board ID"},
+	{ CW_ITEM_WTP_GROUP_NAME,"WTP Group Name"},
+	{ 0,0}
+
+};
