831ffe76ca54677762db414ef446a402d7ccaf3f
7u83 <7u83@mail.ru>
2015-04-19 16:44:49 +0200

    MAVL.

---
+++ src/capwap/mavl_del_all.c
@@ -0,0 +1,36 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "mavl.h"
+
+static void mavl_del_all0(struct mavl *t ,struct mavlnode * n)
+{
+	if (!n)
+		return;
+	mavl_del_all0(t,n->left);
+	mavl_del_all0(t,n->right);
+	mavlnode_destroy(t,n);
+}
+
+void mavl_del_all(struct mavl *t)
+{
+	mavl_del_all0(t,t->root);		
+	t->root=0;
+}
+
+
