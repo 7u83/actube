e01fcbddc49e4384391186fc2754c8ef203b3a36
7u83 <7u83@mail.ru>
2015-04-26 12:36:46 +0200

    Missing ...

---
+++ src/capwap/cipwap_actions.h
@@ -0,0 +1,19 @@
+
+
+#include "capwap_cisco.h"
+
+#define CW_ACTION_IN_CISCO_AP_MODE_AND_TYPE	 	\
+	CW_CISCO_AP_MODE_AND_TYPE,			/* Element ID*/			\
+	cw_in_generic, 0,				/* start/end callback */	\
+	MBAG_WORD, 					/* Type of element */		\
+	CW_ITEM_AP_MODE_AND_TYPE,			/* ID to use store */		\
+	2, 2						/* min/max length */
+
+#define CW_ACTION_IN_CISCO_RADIO_ADMINISTRATIVE_STATE	 	\
+	CW_ELEM_RADIO_ADMINISTRATIVE_STATE,		/* Element ID*/			\
+	cw_in_cisco_radio_administrative_state, 0,		/* start/end callback */	\
+	MBAG_DWORD, 					/* Type of element */		\
+	0,						/* ID to use store */		\
+	4, 4						/* min/max length */
+
+
