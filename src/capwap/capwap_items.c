138c5cc13e661e77a15e6ff0ee2e7d41a68db27e
7u83 <7u83@mail.ru>
2015-04-29 19:33:17 +0200

    First items added.

--- src/capwap/capwap_items.c
+++ src/capwap/capwap_items.c
@@ -2,9 +2,10 @@
 
 #include "capwap_items.h"
 
-struct cw_item wtp_items[] = {
+struct cw_item capwap_itemdefs[] = {
 
 {CW_ITEM_WTP_NAME,"wtp_name",MBAG_BSTR16},
+{CW_ITEM_LOCATION_DATA,"location_data",MBAG_BSTR16},
 
 
 {CW_ITEM_NONE}
