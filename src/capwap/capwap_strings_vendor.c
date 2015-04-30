74db7141dd8890a43b903f5a6e759ac7ae71a915
7u83 <7u83@mail.ru>
2015-04-30 18:46:27 +0200

    Lancom added.

--- src/capwap/capwap_strings_vendor.c
+++ src/capwap/capwap_strings_vendor.c
@@ -5,5 +5,6 @@ struct cw_str capwap_strings_vendor[] = {
 { CW_VENDOR_ID_CISCO, "Cisco" },
 { CW_VENDOR_ID_ZYXEL, "Zyxel" },
 { CW_VENDOR_ID_FSF, "FSF"},
+{ CW_VENDOR_ID_LANCOM, "Lancom"},
 { CW_STR_STOP, "Unknown" }
 };
