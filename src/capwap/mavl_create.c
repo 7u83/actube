773122c671cca54cdb7b7c4e00526113a922580d
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-05-01 17:51:48 +0000

    Comments added.
    FossilOrigin-Name: 755fe180f0bb6045fa6969a12ac86f5d47cf744add2b173293fecd83c2a0056a

--- src/capwap/mavl_create.c
+++ src/capwap/mavl_create.c
@@ -1,3 +1,27 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
+ *@file
+ *@Implementation of mavl_create
+ *@addtogroup Mavl 
+ *@{
+ */ 
 #include "mavl.h"
 
 /**
@@ -19,4 +43,5 @@ struct mavl *mavl_create(int (*cmp) (const void *, const void *),
 	return t;
 }
 
+/**@}*/
 
