c69570713ef75ee3e5a41a5edf7dd3c58d794f4d
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-05-04 05:28:21 +0000

    Comments stuff.
    FossilOrigin-Name: 7d81fc88819b4286912472223025e9afaccba0cd70b038aacecbfbb5806a366f

--- src/capwap/file.h
+++ src/capwap/file.h
@@ -1,9 +1,15 @@
 #ifndef __CW_FILE_H
 #define __CW_FILE_H
 
+/**
+ *@defgroup FileFunctions File Functions
+ *@{
+ */
+
 char *cw_load_file(const char *filename,size_t *size);
 int cw_save_file(const char *filename, char *data,int len);
 
+/**@}*/
 
 #endif
 
