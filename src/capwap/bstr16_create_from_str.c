2990b2cd3bb29bf6b071d1f896e706ebb29d8068
7u83 <7u83@mail.ru>
2015-05-04 07:30:58 +0200

    To read from config files.

---
+++ src/capwap/bstr16_create_from_str.c
@@ -0,0 +1,31 @@
+#include "bstr.h"
+#include "format.h"
+
+
+uint8_t * bstr16_create_from_str(const char *s)
+{
+	int l = strlen(s);
+	if (s[0]!='.')
+		return bstr16_create((uint8_t*)s,l);
+
+	if (l<=2)
+		return bstr16_create((uint8_t*)s,l);
+
+	if (s[1]=='.')
+		return bstr16_create((uint8_t*)s+1,l-1);
+
+	if (s[1]!='x')
+		return bstr16_create((uint8_t*)s,l);
+
+	/* the string starts with ".x" - read hexbytes */
+	l-=2;
+	int msize=l/2;	
+	if(l&1)
+		msize++;
+	uint8_t * mem = malloc(2+msize);
+	*((uint16_t*)mem)=msize;
+	cw_format_scan_hex_bytes(mem+2,s+2,l);
+	return mem;		
+}
+
+
