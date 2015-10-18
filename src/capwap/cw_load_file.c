318953ed5a06c89ebcc50ae6c02927680f0ac4c0
7u83@mail.ru <7u83@mail.ru@noemail.net>
2015-10-18 08:43:40 +0000

    Moved cs_save_file to cw_save_file.c.
    FossilOrigin-Name: 93029d70207fd14bb6d6fc3fbaaef4ad5575386bb0bea9e7a4aa9f16d92cd1f1

--- src/capwap/cw_load_file.c
+++ src/capwap/cw_load_file.c
@@ -1,5 +1,24 @@
+/*
+    This file is part of libcapwap.
+
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libcapwap.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
+
+#include "file.h"
 
 /**
  * @file
@@ -38,19 +57,6 @@ char *cw_load_file(const char *filename, size_t * size)
 	*size = fread(buf, 1, *size, infile);
       errX:
 	fclose(infile);
-
-
 	return buf;
 }
 
-
-int cw_save_file(const char *filename, char *data, int len)
-{
-	FILE *outfile = fopen(filename, "wb");
-	if (!outfile)
-		return 0;
-
-	int bytes = fwrite(data, 1, len, outfile);
-	fclose(outfile);
-	return bytes;
-}
