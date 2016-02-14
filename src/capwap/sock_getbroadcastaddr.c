9d8c31b542e92f2b3302f4b3fb814c294d5152bc
7u83 <7u83@mail.ru>
2016-02-14 08:26:20 +0100

    Added comments.

--- src/capwap/sock_getbroadcastaddr.c
+++ src/capwap/sock_getbroadcastaddr.c
@@ -1,13 +1,37 @@
-#include <string.h>
+/*
+    This file is part of libcapwap.
 
-#include <sys/types.h>
-#include <sys/socket.h>
+    libcapwap is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libcapwap.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ *@file
+ *@brief Implementation of sock_getbroadcastaddr
+ */
+
+
+#include <string.h>
 #include <ifaddrs.h>
 
 #include "sock.h"
 
-
-
+/**
+ * Get the broadcast adddress
+ * @param addr address to search
+ * @param broadcastaddr receives the resulting broadcast address
+ * @return 1 if successful, 0 if no braodcast address was determined
+ */
 int sock_getbroadcastaddr(const struct sockaddr * addr,struct sockaddr * broadcastaddr)
 {
         struct ifaddrs *ifap,*ifa;
