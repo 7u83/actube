7fd7809077d863a97af8652b82f4698adb36ed15
7u83 <7u83@mail.ru>
2016-02-17 16:15:49 +0100

    AC protocols defined.

--- src/ac/ac.h
+++ src/ac/ac.h
@@ -6,3 +6,14 @@ extern int ac_global_init();
 extern mbag_t ac_config;	
 extern struct cw_actiondef capwap_actions;
 
+
+/* the protocol we understand */
+
+enum {
+	AC_PROTO_CAPWAP=0,
+	AC_PROTO_LWAPP=1,
+	AC_PROTO_UNKNOWN
+};
+
+
+
