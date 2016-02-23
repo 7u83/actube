82047233c041ce22afa631c2ee4e94294fb10637
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-02-23 07:58:05 +0000

    Working on mods.
    FossilOrigin-Name: 7652cbc79f78c5605c07225eb1b5500286f3246b61cb1b96734d0f208570ae67

--- src/capwap/mod/cipwap/mod_cipwap_ac.c
+++ src/capwap/mod/cipwap/mod_cipwap_ac.c
@@ -1,5 +1,7 @@
 
 #include "capwap/mod.h"
+#include "capwap/log.h"
+
 #include "mod_cipwap.h"
 
 
@@ -10,10 +12,22 @@ int cipwap_init()
 }
 
 
+static int detect(struct conn *conn,const uint8_t *rawmsg, int rawlen,struct sockaddr *from)
+{
+	cw_log(LOG_INFO,"Detecting ...");
+	return 0;
+}
+
+
+
 static struct mod_ac cipwap_ac = {
+	.name ="cipwap",
 	.init = cipwap_init,
-	.name ="cipwap"
+	.detect = detect
+
 };
+
+
 
 struct mod_ac * mod_cipwap_ac(){
 	return &cipwap_ac;
