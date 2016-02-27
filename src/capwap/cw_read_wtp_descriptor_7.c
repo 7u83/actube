68502c92c439d9fd413b88116b0419e6ce034f92
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-02-27 09:49:06 +0000

    Initial commit
    FossilOrigin-Name: 05e00fccf3948fc30c7f41a4a8d9d7b90b698c924754367c57dfcfc586a55604

---
+++ src/capwap/cw_read_wtp_descriptor_7.c
@@ -0,0 +1,24 @@
+#include "cw.h"
+
+#include "capwap.h"
+#include "capwap_items.h"
+
+/**
+ * Read WTP Descriptor in Cisco-Style (Draft 7)
+ */
+int cw_read_wtp_descriptor_7(mbag_t mbag, struct conn *conn,
+			     struct cw_action_in *a, uint8_t * data, int len)
+{
+
+	mbag_set_byte(mbag, CW_ITEM_WTP_MAX_RADIOS, cw_get_byte(data));
+	mbag_set_byte(mbag, CW_ITEM_WTP_RADIOS_IN_USE, cw_get_byte(data + 1));
+
+
+	int pos = 2;
+
+	/* Encryption element, for now dumy XXX */
+	cw_get_word(data + pos + 2);
+	pos += 2;
+
+	return cw_read_wtp_descriptor_versions(mbag, data + pos, len - pos);
+}
