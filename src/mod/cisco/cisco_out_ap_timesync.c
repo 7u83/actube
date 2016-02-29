1051c716127342cef10c138ddd199ed47de23b64
7u83 <7u83@mail.ru>
2016-02-29 07:49:47 +0100

    Moved files

--- src/mod/cisco/cisco_out_ap_timesync.c
+++ src/mod/cisco/cisco_out_ap_timesync.c
@@ -1,8 +1,10 @@
 
 #include <time.h>
-#include "capwap_cisco.h"
+#include "cisco.h"
 
-int cw_out_cisco_ap_timesync(struct conn *conn,struct cw_action_out * a,uint8_t *dst)
+#include "capwap/capwap_cisco.h"
+
+int cisco_out_ap_timesync(struct conn *conn,struct cw_action_out * a,uint8_t *dst)
 {
 	int l = cw_put_cisco_ap_timesync(dst+10,time(NULL),0);
 	return l + cw_put_elem_vendor_hdr(dst, a->vendor_id, a->elem_id, l);
