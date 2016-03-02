9fff31cb3159e4c833aa828e508aa5647b978250
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-02 18:59:51 +0000

    modload for wtp modules.
    FossilOrigin-Name: 0a9bcd9f1a1682d57293b6ff247247e920afb5546011d490f506857a82374711

---
+++ src/mod/modload_wtp.c
@@ -0,0 +1,23 @@
+#include "../capwap/mod.h"
+
+
+#include "cipwap/mod_cipwap.h"
+#include "capwap/mod_capwap.h"
+#include "cisco/mod_cisco.h"
+
+#define MODS_WTP { mod_capwap_wtp, NULL }
+
+
+struct mod_wtp *(*mods_wtp[])() = MODS_WTP;
+
+struct mod_wtp * modload_wtp(const char *name)
+{
+	int i;
+	for (i=0; mods_wtp[i];i++){
+
+		struct mod_wtp * m = mods_wtp[i]();
+		if (strcmp(m->name,name)==0)
+			return m;
+	}	
+	return NULL;
+}
