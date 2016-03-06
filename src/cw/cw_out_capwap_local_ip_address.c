ec5db6a61a2bea064299c489ae8f3c2f44a612a5
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-06 15:45:56 +0000

    Fixed som IPv4 related bugs.
    FossilOrigin-Name: 17506df7d5864c624b7357667e6e5868ea03094260ed7eeaa9b850f11b59d6b4

--- src/cw/cw_out_capwap_local_ip_address.c
+++ src/cw/cw_out_capwap_local_ip_address.c
@@ -16,6 +16,12 @@
 
 */
 
+/**
+ * @file 
+ * @brief Implementation output handler of capwap cw_out_local_ip_address
+ */
+
+#include <errno.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -29,43 +35,66 @@
 #include "capwap.h"
 #include "conn.h"
 
-int cw_out_capwap_local_ip_address(struct conn *conn, struct cw_action_out *action, uint8_t * dst)
+
+/**
+ * Output handler for Capwap Local IP Address message element.
+ *
+ * @param conn Connection object
+ * @param action Pointer to action which called this handler
+ * @param dst Destination buffer
+ *
+ * This handler determines the IP address from #conn->sock.
+ * It can deal both with IPv4 and IPv6 sockets.
+ *
+ */
+int cw_out_capwap_local_ip_address(struct conn *conn, struct cw_action_out *action,
+				   uint8_t * dst)
 {
-        struct sockaddr_storage a;
-        socklen_t alen = sizeof(struct sockaddr_storage);
-	getsockname (conn->sock,(struct sockaddr *)&a,&alen);
+	struct sockaddr_storage a;
+	socklen_t alen = sizeof(struct sockaddr_storage);
+	int rc = getsockname(conn->sock, (struct sockaddr *) &a, &alen);
+
+	if (rc != 0){
+		cw_log(LOG_ERR,"Can't determine sock IP address - %s\n",strerror(errno));
+		return 0;
+	}
 
 
-	switch (((struct sockaddr*)&a)->sa_family){
+	int id; 
+
+	switch (((struct sockaddr *) &a)->sa_family) {
 		case AF_INET:
 		{
-			struct sockaddr_in  * sain = (struct sockaddr_in*)&a;
-			int id;
-			if (conn->capwap_mode == CW_MODE_CISCO)
-				id = CW_ELEM_WTP_IPV4_IP_ADDRESS; 
+			struct sockaddr_in *sain = (struct sockaddr_in *) &a;
+
+			id  = CW_ELEM_CAPWAP_LOCAL_IPV4_ADDRESS;
+
+/*			if (conn->capwap_mode == CW_MODE_CISCO)
+				id = CW_ELEM_WTP_IPV4_IP_ADDRESS;
 			else
 				id = CW_ELEM_CAPWAP_LOCAL_IPV4_ADDRESS;
-			
-			cw_put_data(dst+4,(uint8_t*)&sain->sin_addr,4);
-			return 4+cw_put_elem_hdr(dst,id,4);
+*/
+			cw_put_data(dst + 4, (uint8_t *) & sain->sin_addr, 4);
+			return 4 + cw_put_elem_hdr(dst, id, 4);
 		}
 
 		case AF_INET6:
 		{
-			int id;
-			if (conn->capwap_mode == CW_MODE_CISCO)
-				id = CW_ELEM_WTP_IPV6_IP_ADDRESS; 
+			id  = CW_ELEM_CAPWAP_LOCAL_IPV6_ADDRESS;
+
+/*			if (conn->capwap_mode == CW_MODE_CISCO)
+				id = CW_ELEM_WTP_IPV6_IP_ADDRESS;
 			else
 				id = CW_ELEM_CAPWAP_LOCAL_IPV6_ADDRESS;
-			struct sockaddr_in6  * sain = (struct sockaddr_in6*)&a;
-			cw_put_data(dst,(uint8_t*)&sain->sin6_addr,16);
-			return 16+cw_put_elem_hdr(dst,id,16);
+*/
+
+			struct sockaddr_in6 *sain = (struct sockaddr_in6 *) &a;
+			cw_put_data(dst + 4, (uint8_t *) & sain->sin6_addr, 16);
+			return 16 + cw_put_elem_hdr(dst, id, 16);
 
 		}
 	}
 
-
+	cw_log(DBG_MSG_ERR, "Can't determine CAPWAP Local IP Adress");
 	return 0;
 }
-
-
