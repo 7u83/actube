475367a71eaab8adb6c70225469ff7474dcc44bf
7u83 <7u83@mail.ru>
2016-03-06 11:00:41 +0100

    Removed some out-commented code, updated license.

--- src/cw/conn_send_packet.c
+++ src/cw/conn_send_packet.c
@@ -1,7 +1,7 @@
 /*
-    This file is part of libcapwap.
+    This file is part of actube.
 
-    libcapwap is free software: you can redistribute it and/or modify
+    actube is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -28,14 +28,6 @@
 
 int conn_send_packet(struct conn * conn, const uint8_t * buffer, int len)
 {
-/*
-#ifdef WITH_CW_LOG_DEBUG
-	char addrstr[64];
-	sock_addrtostr((struct sockaddr*)&conn->addr,addrstr,64);
-	cw_log_debug1("Sending packet to %s, len=%d",addrstr,len);
-	cw_log_debug2_dump(buffer,len,"Packet data for packet, sent to %s",addrstr);
-#endif
-*/	
 	int n;
 	while((n=sendto( conn->sock, buffer, len, 0, 
 				(struct sockaddr*)&conn->addr, 
@@ -43,6 +35,8 @@ int conn_send_packet(struct conn * conn, const uint8_t * buffer, int len)
 
 		if(errno == EINTR) 
 			continue;
+
+
 		return n;
         }
 
