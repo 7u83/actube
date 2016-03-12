63e4a54aaae42d7eec454a52394f06aebc2d9ee4
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-12 21:29:18 +0000

    Copyright notices changed.
    FossilOrigin-Name: 5035088fff15dafbdd31be683d4d43d389625a62d1dde7c5d11c845a8fda35f1

--- src/cw/dtls_bio.c
+++ src/cw/dtls_bio.c
@@ -1,7 +1,7 @@
 /*
-    This file is part of libcapwap.
+    This file is part of actube.
 
-    libcapwap is free software: you can redistribute it and/or modify
+    actube is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
@@ -15,6 +15,7 @@
     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 
 */
+
 
 /**
  * @file
