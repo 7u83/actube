979f410cdf8639c6515d7ec11c680d663ed7f3a8
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-13 00:44:42 +0000

    Moved vendor ids to a separate file.
    FossilOrigin-Name: b54843ce9928ff471e9bcad5c59adb02607c05b4e4731ab8148735d9bebbc4ab

---
+++ src/cw/vendors.h
@@ -0,0 +1,36 @@
+/*
+    This file is part of actube.
+
+    actube is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
+ * @file 
+ * @brief Definition of vendor IDs
+ */
+
+#ifndef __VENDORS_H
+#define __VENDORS_H
+
+#define LW_VENDOR_ID_CISCO	4232704
+
+
+#define CW_VENDOR_ID_FSF	11591
+#define CW_VENDOR_ID_ZYXEL	890
+#define CW_VENDOR_ID_CISCO	LW_VENDOR_ID_CISCO
+#define CW_VENDOR_ID_LANCOM	2356
+#define CW_VENDOR_ID_FORTINET	12356
+
+#endif
