a513c5c8fdad346d3dfbd8b1c0f90db430f64a75
7u83 <7u83@mail.ru>
2016-03-13 18:48:00 +0100

    Strings for board sub-elements.

---
+++ src/cw/capwap_strings_board.c
@@ -0,0 +1,11 @@
+#include "capwap.h"
+
+struct cw_strlist_elem capwap_strings_board[] = {
+	{CW_BOARDDATA_MODELNO, "Model-No"},
+	{CW_BOARDDATA_SERIALNO,"Serial-No"},
+	{CW_BOARDDATA_BOARDID,"Board-ID"},
+	{CW_BOARDDATA_REVISION,"Revision"},
+	{CW_BOARDDATA_MACADDRESS,"MAC-Address"},
+	{CW_STR_STOP, "Unknown" }
+};
+
