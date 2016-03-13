bf9335b969a4c3fc8fa88e117b85caedcb20db3e
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-13 08:36:28 +0000

    Comments added-
    FossilOrigin-Name: de3ae69daea274e0c129fb9dca33b31cdcd38ec42be6b3a4ea9c38e24513fdd5

--- src/cw/bstrv_create_from_str.c
+++ src/cw/bstrv_create_from_str.c
@@ -1,7 +1,35 @@
+/*
+    This file is part of actube.
+
+    actube is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
+ * @file
+ */
+
 #include "bstr.h"
 #include "format.h"
 
-
+/**
+ * Create a bstrv variable from string.
+ * @param vendor_id Vendor ID
+ * @param s string
+ * @return the created bstrv variable or NULL if the 
+ * operation was not sucessfull
+ */
 uint8_t * bstrv_create_from_str(uint32_t vendor_id,const char *s)
 {
 	int l = strlen(s);
