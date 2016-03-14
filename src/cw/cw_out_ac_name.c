c93a6b13ca7564fadfb1982a72c561d265bbac47
7u83 <7u83@mail.ru>
2016-03-14 19:47:25 +0100

    Not needed anymore.

--- src/cw/cw_out_ac_name.c
+++
@@ -1,9 +0,0 @@
-
-#include "capwap.h"
-
-int cw_out_ac_name(uint8_t * dst,cw_item *item)
-{
-	uint8_t *data = item->data;
-	int len = cw_put_data(dst+4,data,strlen(data));
-	return len + cw_put_elem_hdr(str,CW_ELEM_AC_NAME,len);
-}
