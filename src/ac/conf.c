bda2bc8d00f587d2064c91034557fd9ed5b7e19c
Alexis La Goutte <alexis.lagoutte@gmail.com>
2016-03-25 17:02:42 +0100

    Fix some seg fault with venet nic

--- src/ac/conf.c
+++ src/ac/conf.c
@@ -361,6 +361,9 @@ static int init_listen_addrs()
 	/* count the addresses */
 	int ctr = 0;
 	for (ifa = ifap; ifa != 0; ifa = ifa->ifa_next) {
+		if (!ifa->ifa_addr)
+			continue;
+
 		if (ifa->ifa_addr->sa_family == AF_INET && conf_ipv4)
 			ctr++;
 #ifdef WITH_IPV6
@@ -388,6 +391,8 @@ static int init_listen_addrs()
 				continue;
 			}
 		}
+		if (!ifa->ifa_addr)
+			continue;
 
 		if (ifa->ifa_addr->sa_family == AF_INET && conf_ipv4) {
 			sock_addrtostr(ifa->ifa_addr, str, 100,0);
@@ -492,6 +497,8 @@ int init_bcast_addrs()
 
 
 		sa = ifa->ifa_addr;
+		if (!sa)
+			continue;
 		if (sa->sa_family != AF_INET)
 			continue;
 
