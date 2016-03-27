0f2c02b32acb18f5718ada3239a8f600f40efd8f
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-27 01:37:17 +0000

    Getting capwap timers from conn->local.
    FossilOrigin-Name: 005d392bb2d208d345152f1245dea8215aabeca97b4307f3cca35f74c0c1b0e0

--- src/mod/capwap/capwap_out_get_idle_timeout.c
+++ src/mod/capwap/capwap_out_get_idle_timeout.c
@@ -17,9 +17,9 @@ struct mbag_item * capwap_out_get_idle_timeout(struct conn *conn,struct cw_actio
 
 struct mbag_item * capwap_out_get_capwap_timers(struct conn *conn,struct cw_action_out * a)
 {
-	struct mbag_item * m = mbag_get(conn->config,CW_ITEM_CAPWAP_TIMERS);
+	struct mbag_item * m = mbag_get(conn->local,CW_ITEM_CAPWAP_TIMERS);
 	if (m)
 		return m;
 
-	return mbag_set_word(conn->config,CW_ITEM_CAPWAP_TIMERS,CAPWAP_MAX_DISCOVERY_INTERVAL<<8 | CAPWAP_ECHO_INTERVAL);
+	return mbag_set_word(conn->local,CW_ITEM_CAPWAP_TIMERS,CAPWAP_MAX_DISCOVERY_INTERVAL<<8 | CAPWAP_ECHO_INTERVAL);
 }
