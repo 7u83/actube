6d50a52c10009e81dc328ac64a0e364b2d20c9d7
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-27 16:37:15 +0000

    Some comments.
    FossilOrigin-Name: 14299f154e49bddbe9d5d79d4b1ca22c06bb7085c17814f9b17b95eb44d74e23

--- src/cw/mlist.h
+++ src/cw/mlist.h
@@ -1,8 +1,46 @@
+/*
+    This file is part of actube.
+
+    actube is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libcapwap is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #ifndef __MLIST_H
 #define __MLIST_H
 
+/**
+ * @file
+ * @brief Mlist Mini list
+ */
+
+/**
+ * @defgroup MLIST MLIST
+ * @{
+ */
+
+/**
+ * Element of an mlist. 
+ * It's a simple connected list, just with pone connection to the 
+ * next element. 
+ */
 struct mlist_elem {
+	/** Pointer to data */
 	void *data;
+	/** 
+	 * Pointer to next element 
+	 * The last element is determined by a NULL pointer
+	 * */
 	struct mlist_elem *next;
 };
 
@@ -15,10 +53,21 @@ struct mlist {
 typedef struct mlist mlist_t;
 
 
+/**
+ * defgroup MLIST_FUNCTIONS
+ * @{
+ */
 extern mlist_t *mlist_create(int (*cmp) (void *v1, void *v2));
 extern struct mlist_elem *mlist_append(mlist_t * l, void *data);
 extern struct mlist_elem *mlist_find(mlist_t * l, struct mlist_elem *start, void *data);
 extern struct mlist_elem *mlist_replace(mlist_t *l, struct mlist_elem *start, void *data);
 
+/**
+ * @}
+ */
+
+/**
+ * @}
+ */
 
 #endif
