671d190c948ff7086e48a0fba64285e6311ab08a
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-28 00:01:55 +0000

    m,ore strings.
    FossilOrigin-Name: ba6f4f97a6488b9377b2b6affe7157b0320728158a10a7fd879fcdfa6b909bf0

--- src/cw/capwap_strings_elem80211.c
+++ src/cw/capwap_strings_elem80211.c
@@ -5,7 +5,9 @@ struct cw_strlist_elem capwap_strings_elem80211[] = {
 
 	{CW_ELEM80211_ADD_WLAN, "802.11 Add WLAN"},
 	{CW_ELEM80211_ANTENNA, "802.11 Antenna"},
-	{CW_ELEM80211_RATE_SET, " 802.11 Rate Set"},
+	{CW_ELEM80211_RATE_SET, "802.11 Rate Set"},
+	{CW_ELEM80211_SUPPORTED_RATES,"802.11 Supported Rates"},
+
 	{CW_ELEM80211_WTP_RADIO_INFORMATION, " 802.11 WTP Radio Information"},
 	{CW_STR_STOP, " Unknown "}
 
