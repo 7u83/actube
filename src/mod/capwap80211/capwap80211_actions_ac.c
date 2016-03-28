bddf9fd47e4bb3d42db47383fdeb8dc3e3faec97
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-28 14:48:32 +0000

    Using CWITEM80211_RADIO ....
    FossilOrigin-Name: e7c9e98b506820092f0818e9811e38b09c389bb0f23782af501446449bb38981

--- src/mod/capwap80211/capwap80211_actions_ac.c
+++ src/mod/capwap80211/capwap80211_actions_ac.c
@@ -3,8 +3,11 @@
 #include "cw/action.h"
 #include "cw/cw.h"
 #include "cw/capwap.h"
-#include "cw/capwap80211.h"
 #include "cw/capwap_items.h"
+
+#include "cw/capwap80211.h"
+#include "cw/capwap80211_items.h"
+
 
 //#include "capwap_80211_actions.h"
 //#include "capwap_80211.h"
@@ -26,7 +29,7 @@ static cw_action_in_t actions_ac_in[] = {
 		.capwap_state = CW_STATE_DISCOVERY, 
 		.msg_id = CW_MSG_DISCOVERY_REQUEST, 
 		.elem_id = CW_ELEM80211_WTP_RADIO_INFORMATION,
-		.item_id = "radio_information",
+		.item_id = CW_ITEM80211_WTP_RADIO_INFORMATION,
 	 	.start = cw_in_radio_generic, 
 		.mand = 1, 
 		.min_len = 5, 
@@ -44,7 +47,7 @@ static cw_action_in_t actions_ac_in[] = {
 		.capwap_state = CW_STATE_JOIN, 
 		.msg_id = CW_MSG_JOIN_REQUEST, 
 		.elem_id = CW_ELEM80211_WTP_RADIO_INFORMATION,
-		.item_id = "radio_information",
+		.item_id = CW_ITEM80211_WTP_RADIO_INFORMATION,
 	 	.start = cw_in_radio_generic, 
 		.mand = 1, 
 		.min_len = 5, 
@@ -61,7 +64,7 @@ static cw_action_in_t actions_ac_in[] = {
 		.capwap_state = CW_STATE_CONFIGURE, 
 		.msg_id = CW_MSG_CONFIGURATION_STATUS_REQUEST, 
 		.elem_id = CW_ELEM80211_SUPPORTED_RATES,
-		.item_id = "supported_rates",
+		.item_id = CW_ITEM80211_SUPPORTED_RATES,
 	 	.start = cw_in_radio_generic, 
 		.mand = 1, 
 		.min_len = 3, 
@@ -87,7 +90,7 @@ cw_action_out_t actions_ac_out[]={
 	/* 802.11 Radio Information - Discovery Response */
 	{
 		.msg_id = CW_MSG_DISCOVERY_RESPONSE, 
-		.item_id = CW_ITEM_RADIO_INFOS ,
+		.item_id = CW_ITEM80211_WTP_RADIO_INFORMATION ,
 	 	.elem_id = CW_ELEM80211_WTP_RADIO_INFORMATION, 
 		.out = cw_out_radio_infos, 
 		.mand = 1
@@ -102,7 +105,7 @@ cw_action_out_t actions_ac_out[]={
 	/* 802.11 Radio Information - Discovery Response */
 	{
 		.msg_id = CW_MSG_JOIN_RESPONSE, 
-		.item_id = CW_ITEM_RADIO_INFOS ,
+		.item_id = CW_ITEM80211_WTP_RADIO_INFORMATION,
 	 	.elem_id = CW_ELEM80211_WTP_RADIO_INFORMATION, 
 		.out = cw_out_radio_infos, 
 		.mand = 1
@@ -130,6 +133,9 @@ int capwap80211_register_actions_ac(struct cw_actiondef *def)
 	rc+=cw_actionlist_out_register_actions(def->out, actions_ac_out);
 
 	rc+= cw_strheap_register_strings(def->strelem, capwap_strings_elem80211);
+
+	rc += cw_itemdefheap_register(def->items,capwap80211_itemdefs);
+
 	/*rc += cw_strheap_register_strings(def->strelem, capwap_strings_elem);
 */
 
