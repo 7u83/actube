a0db6068a184942bec09e98d214bfce83ef7fcb2
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-03-28 09:10:20 +0000

    Put method added.
    FossilOrigin-Name: 219a26f64ffba7f2484aa60229c549161ba03464f6e5b67a336ad42826601f18

--- src/cw/mbag.h
+++ src/cw/mbag.h
@@ -58,6 +58,9 @@ struct mbag_typedef{
 	/** Cereate an item of this type from a string, which was previously
 	    created by the #del function. */
 	struct mbag_item * (*from_str)(const char *src);
+	/** A method to put this object to a buffer */
+	int (*put)(void *,uint8_t*dst);
+
 };
 
 /** The type for an mbag typedef */
