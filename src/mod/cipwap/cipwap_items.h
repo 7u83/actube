8454196e0c98e1d30702eddf14c20d7ac6b68ecc
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-04-02 15:41:02 +0000

    Correct naming
    FossilOrigin-Name: 7a403009c3679c7ac32b5167045cd300dfaa655dba43832c9e83248a295959ff

--- src/mod/cipwap/cipwap_items.h
+++ src/mod/cipwap/cipwap_items.h
@@ -5,8 +5,8 @@ extern const char CIPWAP_ITEM_SSH_ENABLE[];
 extern const char CIPWAP_ITEM_TELNET_ENABLE[];
 extern const char CIPWAP_ITEM_AP_HASH_VALUE[];
 
-extern const char CIPWAP_RADIO80211ITEM_OCCUPANCY_LIMIT[];
-extern const char CIPWAP_RADIO80211ITEM_CFP_PERIOD[];
+extern const char CIPWAP_RADIOITEM80211_OCCUPANCY_LIMIT[];
+extern const char CIPWAP_RADIOITEM80211_CFP_PERIOD[];
 
 extern struct cw_itemdef cipwap_itemdefs[];
 extern struct cw_itemdef cipwap_radioitemdefs[];
