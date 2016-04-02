9876ce40c2b243786d70929657d99e037a0f91e5
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-04-02 08:23:29 +0000

    Radio items added.
    FossilOrigin-Name: e3acfdac2d9a2f811d11fbdcbc36582c4d2d709457e951812e710549b74f8ccf

--- src/cw/capwap80211_items.c
+++ src/cw/capwap80211_items.c
@@ -16,6 +16,11 @@ struct cw_itemdef capwap80211_itemdefs[] = {
 const char CW_RADIOITEM80211_SUPPORTED_RATES[]="802.11 supported_rates";
 const char CW_RADIOITEM80211_WTP_RADIO_INFORMATION[]="802.11 radio_info";
 
+/* MAC Operation Items */
+const char CW_RADIOITEM80211_RTS_THRESHOLD[]="rts_threshold";
+const char CW_RADIOITEM80211_FRAGMENTATION_THRESHOLD[]="fragmentation_threshold";
+const char CW_RADIOITEM80211_SHORT_RETRY[]="short_retry";
+const char CW_RADIOITEM80211_LONG_RETRY[]="long_retry";
 
 
 
