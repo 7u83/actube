937d29ae24e76f3a622ebe906d924e07d7f4e207
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-04-02 14:51:03 +0000

    cipwap_wtp
    FossilOrigin-Name: bfed87b2cc055f848a60a8d8d9fc6e0683f9e2bd15fff3b3a263ead68a00bc38

--- src/mod/modload.h
+++ src/mod/modload.h
@@ -13,6 +13,6 @@ struct mod_wtp * modload_wtp(const char *name);
 #include "capwap80211/mod_capwap80211.h"
 
 #define MODS_AC { mod_capwap_ac, mod_cipwap_ac, mod_cisco_ac, mod_capwap80211_ac, mod_fortinet_ac, NULL }
-#define MODS_WTP { mod_capwap_wtp, mod_cisco_wtp, mod_capwap80211_wtp, mod_fortinet_wtp, NULL }
+#define MODS_WTP { mod_capwap_wtp, mod_cipwap_wtp, mod_cisco_wtp, mod_capwap80211_wtp, mod_fortinet_wtp, NULL }
 
 
