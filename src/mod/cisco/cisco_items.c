224175758efeebb3bdd260e7429c700691f267e6
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-04-02 12:26:20 +0000

    MWAR Hash Value
    FossilOrigin-Name: 347346759b484a424e8157cb1e59a7f07d39afcff6a4cdddbf033b4b0b491db1

--- src/mod/cisco/cisco_items.c
+++ src/mod/cisco/cisco_items.c
@@ -2,8 +2,12 @@
 #include "cisco_items.h"
 
 const char CISCO_ITEM_AP_MODE_AND_TYPE[]="cisco_ap_mode_and_type";
+const char CISCO_MWAR_HASH_VALUE[]="cisco_mwar_hash_value";
 
 struct cw_itemdef cisco_itemdefs[] = {
+
 	{CISCO_ITEM_AP_MODE_AND_TYPE,CW_ITEM_NONE,MBAG_WORD},
+	{CISCO_MWAR_HASH_VALUE,CW_ITEM_NONE,MBAG_BSTR},
+
 	{CW_ITEM_NONE}
 };
--- src/mod/cisco/cisco_items.h
+++ src/mod/cisco/cisco_items.h
@@ -2,6 +2,7 @@
 #define __CISCO_ITEMS_H
 
 extern const char CISCO_ITEM_AP_MODE_AND_TYPE[];
+extern const char CISCO_MWAR_HASH_VALUE[];
 
 extern struct cw_itemdef cisco_itemdefs[];
 
