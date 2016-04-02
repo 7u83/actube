d5221321c13189d961103443c2a397003b6dd5c8
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-04-02 07:20:40 +0000

    Cisco items
    FossilOrigin-Name: 719c044a9b9c7a3ac5996f4256be618cca0ba150e18da11cf97e70a63ea2d315

---
+++ src/mod/cisco/cisco_items.c
@@ -0,0 +1,9 @@
+#include "cw/item.h"
+#include "cisco_items.h"
+
+const char CISCO_ITEM_AP_MODE_AND_TYPE[]="cisco_ap_mode_and_type";
+
+struct cw_itemdef cisco_itemdefs[] = {
+	{CISCO_ITEM_AP_MODE_AND_TYPE,CW_ITEM_NONE,MBAG_WORD},
+	{CW_ITEM_NONE}
+};
---
+++ src/mod/cisco/cisco_items.h
@@ -0,0 +1,8 @@
+#ifndef __CISCO_ITEMS_H
+#define __CISCO_ITEMS_H
+
+extern const char CISCO_ITEM_AP_MODE_AND_TYPE[];
+
+extern struct cw_itemdef cisco_itemdefs[];
+
+#endif
