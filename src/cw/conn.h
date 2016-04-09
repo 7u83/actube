30492fce39b3f727d0447fbe136e0d6693d06143
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-04-09 16:13:48 +0000

    moved oout connlist stuff
    FossilOrigin-Name: 4a20d87aed9f0ff51f4212bf0c7e4d6ab5e696694787873bffeaef4590075931

--- src/cw/conn.h
+++ src/cw/conn.h
@@ -247,25 +247,6 @@ extern struct cwrmsg *conn_get_response(struct conn *conn);
 #define conn_get_last_seqnum(conn) (conn->seqnum&0xff)
 
 
-/* connlist stuff */
-#include "avltree.h"
-struct connlist {
-/*	struct conn ** connlist; */
-
-	struct avltree *t;
-	int len;
-	pthread_mutex_t connlist_mutex;
-};
-
-
-struct connlist *connlist_create(int len);
-void connlist_lock(struct connlist *cl);
-void connlist_unlock(struct connlist *cl);
-void conlist_destroy(struct connlist *cl);
-struct conn *connlist_get(struct connlist *cl, const struct sockaddr *addr);
-struct conn *connlist_add(struct connlist *cl, struct conn *conn);
-void connlist_remove(struct connlist *cl, struct conn *conn);
-void connlist_destroy(struct connlist *cl);
 void conn_q_add_packet(struct conn *conn, uint8_t * packet, int len);
 
 struct image_identifier;
