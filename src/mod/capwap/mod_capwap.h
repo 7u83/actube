456bb4dc75fcefc665706f259be504ea773dbd6d
7u83@mail.ru <7u83@mail.ru@noemail.net>
2016-04-10 14:03:02 +0000

    Prototypes
    FossilOrigin-Name: fbcce18f88db5c3b2efbedcd2b4a1d50dc48f0138eb4f089f919f0a73c1db868

--- src/mod/capwap/mod_capwap.h
+++ src/mod/capwap/mod_capwap.h
@@ -14,6 +14,9 @@ extern int capwap_in_wtp_board_data(struct conn *conn, struct cw_action_in *a,
 extern int capwap_out_wtp_descriptor(struct conn *conn, struct cw_action_out *a,
 				     uint8_t * dst);
 
+extern int capwap_in_session_id(struct conn *conn, struct cw_action_in *a, uint8_t * data,
+			 int len, struct sockaddr *from);
+
 extern int capwap_out_ac_descriptor(struct conn *conn,struct cw_action_out * a,uint8_t *dst);
 
 extern struct mbag_item * capwap_out_get_session_id(struct conn *conn,struct cw_action_out * a);
