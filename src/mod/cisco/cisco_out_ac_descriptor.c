7ca5cb604493814df1b532cbf42ece1d2d847a3c
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-02-19 02:31:56 +0000

    Send software version from local config
    FossilOrigin-Name: 4f06ab7e20a2866a3d5f0af92b97b5e31ef279c5ab27cdb310d00581d50d5858

--- src/mod/cisco/cisco_out_ac_descriptor.c
+++ src/mod/cisco/cisco_out_ac_descriptor.c
@@ -5,6 +5,7 @@
 #include "cw/capwap_items.h"
 #include "cw/cw.h"
 
+extern mbag_t cisco_config;
 
 int cisco_out_ac_descriptor(struct conn *conn,struct cw_action_out * a,uint8_t *dst) 
 {
@@ -21,12 +22,13 @@ int cisco_out_ac_descriptor(struct conn *conn,struct cw_action_out * a,uint8_t *
 	d+=cw_put_ac_status(d ,(struct cw_ac_status*)(i->data),conn);
 
 	
-	/* Send back the same software as the WTP has,
+	/* Send back the same software version as the WTP has,
 	   otherwise the AP wants us to send an image */
 
-	i = mbag_get(conn->incomming,CW_ITEM_WTP_SOFTWARE_VERSION);
+	//i = mbag_get(conn->incomming,CW_ITEM_WTP_SOFTWARE_VERSION);
+	
 
-	//i = mbag_get(conn->local,CW_ITEM_AC_SOFTWARE_VERSION);
+	i = mbag_get(cisco_config,CW_ITEM_AC_SOFTWARE_VERSION);
 	if ( i ) {	
 	 	d += cw_put_version(d,1,i->data);
 	}
@@ -34,15 +36,13 @@ int cisco_out_ac_descriptor(struct conn *conn,struct cw_action_out * a,uint8_t *
 		cw_log(LOG_ERR, "Can't set Cisco Software Version in AC descriptor, No value defined.");
 	}
 
-	i = mbag_get(conn->local,CW_ITEM_AC_HARDWARE_VERSION);
+	i = mbag_get(cisco_config, CW_ITEM_AC_HARDWARE_VERSION);
 	if ( i ) {	
 	 	d += cw_put_version(d,0,i->data);
 	}
 	else {
 		cw_log(LOG_ERR, "Can't set Cisco Hardware Version in AC descriptor, No value defined.");
 	}
-
-
 
 
 	int len = d-dst-4;
