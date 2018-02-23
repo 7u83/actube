cea7e10c371a86353391152106bad25049465f49
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-02-23 00:43:17 +0000

    Detailed debug output
    FossilOrigin-Name: 212261d50e499aa03f1279a74aaff582264517d125f4447d718e6b8d5537a714

--- src/mod/cisco/cisco_in_radio_operational_state.c
+++ src/mod/cisco/cisco_in_radio_operational_state.c
@@ -16,7 +16,6 @@ int cisco_in_radio_operational_state(struct conn *conn, struct cw_action_in *a, 
 	int state = cw_get_byte(data+1); 
 	int cause = cw_get_byte(data+2);
 
-	cw_dbg (DBG_X,"Read State %d",state);
 
 	int state_t = state;
 	if (state == CW_RADIO_OPER_STATE_ENABLED_7)
@@ -24,7 +23,6 @@ int cisco_in_radio_operational_state(struct conn *conn, struct cw_action_in *a, 
 	if (state == CW_RADIO_OPER_STATE_DISABLED_7)
 		state_t = CW_RADIO_OPER_STATE_DISABLED;
 
-	cw_dbg(DBG_X,"%d Translated state to %d",rid,state_t);
 
 	if (rid==255){
 		mbag_set_word(conn->incomming,CISCO_ITEM_WTP_OPER_STATE, (state_t<<8)|cause);
@@ -38,15 +36,11 @@ int cisco_in_radio_operational_state(struct conn *conn, struct cw_action_in *a, 
 		return 0;
 	}
 
-cw_dbg(DBG_X,"Radio settng to %d",state_t << 8);
+	char * strstate = state_t == CW_RADIO_OPER_STATE_ENABLED ? "enabled":"disabled";
+	cw_dbg(DBG_ELEM_DETAIL,"\tDetail: Radio ID=%d, State=%s, Cause: %d",rid,strstate, cause);
 
 	mbag_set_word(radio,CW_RADIOITEM_OPER_STATE,(state_t<<8)|cause);
-//	mbag_set_word(radio,"hitler_status",911);
-//	mbag_set_word(radio,CW_RADIOITEM_OPER_STATE,912);
 
-cw_dbg(DBG_X, "setting operstate %s",CW_RADIOITEM_OPER_STATE);
-
-//	mbag_set_word(radio,CW_RADIOITEM_ADMIN_STATE,66);
 	return 1;
 
 
