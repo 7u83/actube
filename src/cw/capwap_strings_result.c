4dd1c09b31aa1dbb3ddeafa88c4810f28f422d6f
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-02-26 21:37:39 +0000

    Rename CW_ -> CAPWAP_
    FossilOrigin-Name: bd11d09cffd11569b59bcafd532153ff9f5332a3b310d207459518a41f90a800

--- src/cw/capwap_strings_result.c
+++ src/cw/capwap_strings_result.c
@@ -9,7 +9,7 @@ struct cw_strlist_elem capwap_strings_result[] = {
 	{CW_RESULT_RESET_UNABLE_TO_RESET,"Reset Failure - Unable to Reset"},		/* 10 */
 	{CW_RESULT_IMAGE_DATA_INVALID_CHECKSUM,"Image Data Error (Invalid Checksum)"},			/* 14 */
 	{CW_RESULT_IMAGE_DATA_ERROR,"Image Data Error (Unspecified)"},					/* 16 */
-	{CW_RESULT_MSG_INVALID_IN_CURRENT_STATE,"Message unexpected (Invalid in current state)"},	/* 18 */
+	{CAPWAP_RESULT_MSG_INVALID_IN_CURRENT_STATE,"Message unexpected (Invalid in current state)"},	/* 18 */
 	{CW_RESULT_MISSING_MAND_ELEM,"Missing Mandatory Message Element"},		/* 20 */
 	{CW_RESULT_UNRECOGNIZED_MESSAGE_ELEMENT,"Unrecognized Message Element"},	/* 21 */
 	{CW_STR_STOP,"Unknown Result Code"}
