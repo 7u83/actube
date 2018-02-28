a2851cdc4c19061348aa7234e9130d1afcc672b9
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-02-28 08:46:08 +0000

    new simplified Makefile
    FossilOrigin-Name: a1012388abc39936cf1bf5f7ff48bc721b7983e2342313d7d82afecbd486b267

--- src/cw/capwap_strings_item.c
+++
@@ -1,9 +0,0 @@
-struct cw_str capwap_strings_item[] = {
-
-	{CW_ITEM_AC_DESCRIPTOR, "AC Descriptor"},
-	{CW_ITEM_AC_IPV4_LIST, "AC IPv4 List"},
-	{CW_ITEM_WTP_DESCRIPTOR,"WTP Descriptor"},
-
-	{CW_STR_STOP,"Unknown"}
-
-}
