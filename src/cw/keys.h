9d28e72a6b9acb280cead729819c2c2f78429bcf
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-03-11 09:34:51 +0000

    new file
    FossilOrigin-Name: fa03f58e2053d40b9dd1aaa0a9a1a4f267340816875dc362cb713522f68d6770

---
+++ src/cw/keys.h
@@ -0,0 +1,13 @@
+#ifndef __KEYS_H
+#define __KEYS_H
+
+#define CW_KEY_HARDWARE "hardware"
+#define CW_KEY_SOFTWARE "software"
+#define CW_KEY_BOOTLOADER "bootloader"
+#define CW_KEY_OTHER_SOFTWARE "other_software"
+
+#define CW_KEY_VENDOR "vendor"
+#define CW_KEY_VERSION "version"
+
+#endif
+
