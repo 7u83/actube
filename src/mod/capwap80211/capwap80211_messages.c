3f06e4c0dbf267e45b796a66937dd36bcd7e4855
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-03-26 18:50:18 +0000

    Added nmissing stoppers
    FossilOrigin-Name: 78b897b12ce0571ca669fb163e20518a34902eb1557b8a76b60cf6f11db58136

--- src/mod/capwap80211/capwap80211_messages.c
+++ src/mod/capwap80211/capwap80211_messages.c
@@ -12,52 +12,50 @@
 
 
 static struct cw_ElemHandler handlers[] = {
-	{ 
-		"80211 WTP Radio Information",		/* name */
-		CAPWAP80211_ELEM_WTP_RADIO_INFORMATION,	/* Element ID */
-		0,0,					/* Vendor / Proto */
-		0,0,					/* min/max length */
-		NULL,					/* type */
-		"key",					/* Key */
-		NULL,					/* get */
-		NULL					/* put */
-	}
+	{
+	 "80211 WTP Radio Information",	/* name */
+	 CAPWAP80211_ELEM_WTP_RADIO_INFORMATION,	/* Element ID */
+	 0, 0,			/* Vendor / Proto */
+	 0, 0,			/* min/max length */
+	 NULL,			/* type */
+	 "wtp-radio_information",	/* Key */
+	 NULL,			/* get */
+	 NULL			/* put */
+	 },
+	{NULL, 0, 0, 0, 0, 0, NULL, NULL, NULL, NULL}
+
+
 };
 
-static int discovery_request_states[] = {CAPWAP_STATE_DISCOVERY,0};
-static struct cw_ElemDef discovery_request_elements[] ={
-	{0,0,CAPWAP80211_ELEM_WTP_RADIO_INFORMATION,	1, 0},
-	{0,0,0,0,0}
+static int discovery_request_states[] = { CAPWAP_STATE_DISCOVERY, 0 };
+
+static struct cw_ElemDef discovery_request_elements[] = {
+	{0, 0, CAPWAP80211_ELEM_WTP_RADIO_INFORMATION, 1, 0},
+	{0, 0, 0, 0, 0}
 
 };
 
 static struct cw_MsgDef messages[] = {
 	{
-		"Discovery Request",
-		CAPWAP_MSG_DISCOVERY_REQUEST,
-		CW_RECEIVER_AC,
-		discovery_request_states,
-		discovery_request_elements
-	}
+	 "Discovery Request",
+	 CAPWAP_MSG_DISCOVERY_REQUEST,
+	 CW_RECEIVER_AC,
+	 discovery_request_states,
+	 discovery_request_elements},
+	{0, 0, 0, 0, 0}
 };
 
-
-
-
-
-
-
-struct cw_MsgSet * capwap80211_register_msg_set(set, mode)
-	struct cw_MsgSet * set;
-	int mode;
+struct cw_MsgSet *capwap80211_register_msgset(set, mode)
+struct cw_MsgSet *set;
+int mode;
 {
 	if (mode != CW_MOD_MODE_BINDINGS)
 		return NULL;
-	
-	cw_dbg(DBG_INFO,"CAPWAP80211: Register messages");
-	
-	cw_msgset_add(set,messages, handlers);
-	cw_dbg(DBG_INFO,"CAPWAP0211: Done register messages");
-	
+
+	cw_dbg(DBG_INFO, "CAPWAP80211: Register messages");
+
+	cw_msgset_add(set, messages, handlers);
+	cw_dbg(DBG_INFO, "CAPWAP0211: Done register messages");
+
 	return set;
 }
