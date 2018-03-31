2734c8a8c425456cc6fb9524119a83f48fc2142e
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-03-31 08:12:02 +0000

    Reformatting ...
    FossilOrigin-Name: d973e07fa8241230f4dabbbe4aa7a6586bc4567c278ac0a96d182a9904493304

--- src/cw/cw_ktv_mavldel.c
+++ src/cw/cw_ktv_mavldel.c
@@ -7,10 +7,8 @@
 void cw_ktv_mavldel(void *data)
 {
 	struct cw_KTV *ktv = data;
-	/*printf("KTV Del Key,Type: %s %s\n", ktv->key,ktv->type->name);*/
-	if (ktv->type->del){
+	if (ktv->type->del) {
 		ktv->type->del(data);
 	}
-
 	free(ktv->key);
 }
