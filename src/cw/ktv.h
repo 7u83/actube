72880ccd25890b618f14bb5a6fb85a9b051b4cce
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-04-01 08:06:43 +0000

    new function prototype cw_ktv_get_str
    FossilOrigin-Name: 20d21fc3409411a74b500b359bd7a7a88b48aa4a3b8bf984c3996bab784cd165

--- src/cw/ktv.h
+++ src/cw/ktv.h
@@ -139,6 +139,7 @@ uint8_t cw_ktv_get_byte(mavl_t ktv,const char *key, uint8_t def);
 uint16_t cw_ktv_get_word(mavl_t ktv,const char *key, uint16_t def);
 uint32_t cw_ktv_get_dword(mavl_t ktv,const char *key, uint32_t def);
 void * cw_ktv_get_sysptr(mavl_t ktv,const char *key, void * def);
+char * cw_ktv_get_str(mavl_t ktv,const char *key, char * def);
 
 int cw_ktv_idx_get(mavl_t ktv, const char *key, int idx, const cw_Type_t * type);
 
