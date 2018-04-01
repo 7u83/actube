1f22989834093ddf572b198969be28ccb5a0e5fe
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-04-01 08:05:01 +0000

    function mame corrected
    FossilOrigin-Name: d76207257a78f55e7f668edf0adf377e360ec1fc4bb3f7a5f4acec9a5b03258f

--- src/cw/cw_ktv_get_str.c
+++ src/cw/cw_ktv_get_str.c
@@ -1,6 +1,6 @@
 #include "ktv.h"
 
-char * cw_ktv_get_syst(mavl_t ktv,const char *key, void * def)
+char * cw_ktv_get_str(mavl_t ktv,const char *key, char * def)
 {
 	cw_KTV_t * k;
 	k = cw_ktv_get(ktv,key,CW_TYPE_STR);
