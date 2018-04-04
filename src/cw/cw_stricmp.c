fd893751ee87a6bfd3156c6a0ccb167a4fc52205
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-04-04 07:01:28 +0000

    Add function cw_stricmp
    FossilOrigin-Name: b11dc8075a7ca248bff1e0afa5277b70ae7703d85bc0b16b717e5b6e6fc04e45

---
+++ src/cw/cw_stricmp.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+
+#include "cw.h"
+
+int cw_strcicmp(char const *a, char const *b)
+{
+    for (;; a++, b++) {
+        int d = tolower(*a) - tolower(*b);
+        if (d != 0 || !*a)
+            return d;
+    }
+}
