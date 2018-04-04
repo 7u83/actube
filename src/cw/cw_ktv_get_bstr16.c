76582802d2df407b4a511ad68d6ee2b748a8fc79
7u83@mail.ru <7u83@mail.ru@noemail.net>
2018-04-04 07:59:55 +0000

    Fixed type
    FossilOrigin-Name: bc432d8160d7cae5e6cbb57ad3a5eebfeb94f000c1c13f73c15ae5162778f2c4

--- src/cw/cw_ktv_get_bstr16.c
+++ src/cw/cw_ktv_get_bstr16.c
@@ -4,7 +4,7 @@
 bstr16_t cw_ktv_get_bstr16(mavl_t ktv,const char *key, bstr16_t def)
 {
 	cw_KTV_t * k;
-	k = cw_ktv_get(ktv,key,CW_TYPE_STR);
+	k = cw_ktv_get(ktv,key,CW_TYPE_BSTR16);
 	if (k != NULL){
 		return bstr16_create(k->type->data(k),k->type->len(k));
 	}
