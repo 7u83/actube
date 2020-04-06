0f6cfb9eab2843cf8269187d64cc797491d55b02
root <root@noemail.net>
2020-04-06 22:23:50 +0000

    qos in add wlan element added
    FossilOrigin-Name: 7eb533db72ded8c6cc1aadf337a3ab527f07157a15a3f4e9fd5f1905885e7e27

--- src/mod/cisco/cisco_actions_ac.c
+++ src/mod/cisco/cisco_actions_ac.c
@@ -481,16 +481,19 @@ static cw_KTVStruct_t cisco_add_wlan[]={
 static cw_KTVStruct_t cisco_add_wlan70[]={
 	{CW_TYPE_BYTE,"radio-id",1,-1},
 	{CW_TYPE_WORD,"wlan-capability",2,-1},
-	{CW_TYPE_BYTE,"wlan-id",1,-1},
-	{CW_TYPE_DWORD,"encryption-policy",4,-1},
+	{CW_TYPE_BYTE,"wlan-id",1,4},
+	{CW_TYPE_DWORD,"encryption-policy",4,5},
 
 	{CW_TYPE_BSTR16,"wep-key",13,9},
 	{CW_TYPE_BYTE,"wep-key-index",1,41},
 	{CW_TYPE_BOOL,"wep-encryption",1,42},
-	
+
+	{CW_TYPE_BYTE,"qos",1,324},
+	{CW_TYPE_WORD,"scan-defer-time",1,330},
 	{CW_TYPE_BOOL,"broadcast-ssid",1,332},
 	{CW_TYPE_WORD,"session-timout",2,381},
 
+	{CW_TYPE_BYTE, "dtim-period",1,440},
 	{CW_TYPE_STR,"profile-name",30,441},
 	{CW_TYPE_STR, "ssid",-1,480},
 
